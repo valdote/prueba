2ca52166c8f6c1d15153c4beca7ac3bf4ef9c59c
Osvaldo Flores <osvaldo.delgadofl@gmail.com>
2020-03-03 14:34:09 -0600

    sayHello function added

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 
+void sayHello(char []);
+
 int main(){
-	printf("hello world.\n");
+	sayHello("Marco");
 	return 0;
+}
+
+void sayHello(char name[40]) {
+	printf("Hello %s", name);
 }
